5cc1cd5f2f4d5a7c0ef9cb66884b82f6d33db7eb
Issabek <180107287@stu.sdu.edu.kz>
2021-03-14 20:37:46 +0600

    done

---
+++ WorldOfTanks/Tank.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace WorldOfTanks
+{
+    public abstract class Tank
+    {
+        public int Ammunition { get; set; }
+        public int Armour { get; set; }
+        public int Mobility { get; set; }
+        public string Name { get; set; }
+
+        public string ShowInfo()
+        {
+            return string.Format("Tank name: {0};\nAmmunition status: {1}%;\nArmour status: {2}%;\nMobility status: {3}%;", Name, Ammunition, Armour, Mobility); ;
+        }
+
+        public static bool operator <(Tank someTank, Tank otherTank)
+        {
+            int result = ((someTank.Mobility - otherTank.Mobility) > 0 ? 1 : -1) + ((someTank.Armour - otherTank.Armour) > 0 ? 1 : -1) + ((someTank.Ammunition - otherTank.Ammunition) > 0 ? 1 : -1);
+            if (result > 0)
+                return false;
+            else
+                return true;
+        }
+
+        public static bool operator >(Tank someTank, Tank otherTank)
+        { 
+            int result = ((someTank.Mobility - otherTank.Mobility) > 0 ? 1 : -1) + ((someTank.Armour - otherTank.Armour) > 0 ? 1 : -1) + ((someTank.Ammunition - otherTank.Ammunition) > 0 ? 1 : -1);
+            if (result > 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static Tank operator ^(Tank someTank, Tank otherTank)
+        {
+            if (someTank.Name == null || otherTank.Name==null)
+            {
+                throw new Exception("Tank's \"Name\" property cannot be null or empty");
+            }
+            else
+            {
+                if (someTank > otherTank)
+                    return someTank;
+                else
+                    return otherTank;
+            }
+        }
+    }
+
+    public class T34 : Tank
+    {
+        public T34()
+        {
+            Random rnd = new Random();
+            Ammunition = rnd.Next(50,100);
+            Armour = rnd.Next(50,100);
+            Mobility = rnd.Next(50,100);
+        }
+
+        public T34(string Name) : this()
+        {
+            this.Name = Name;
+        }
+    }
+
+    public class PANTHER : Tank
+    {
+        public PANTHER()
+        {
+            Random rnd = new Random();
+            Ammunition = rnd.Next(50,100);
+            Armour = rnd.Next(50,100);
+            Mobility = rnd.Next(50,100);
+        }
+
+        public PANTHER(string Name) : this()
+        {
+            this.Name = Name;
+        }
+    }
+
+}
---
+++ itSTEP_6/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using WorldOfTanks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+namespace itSTEP_6
+{
+
+    class Program
+    {
+        public static void TankWar(List<T34> t34s, List<PANTHER> panthers)
+        {
+            Console.Clear();
+            int counter = 1;
+            Console.WriteLine("Starting fight!");
+            var T34AndPanther = t34s.Zip(panthers, (t, p) => new { t34 = t, panther = p });
+            foreach(var pair in T34AndPanther)
+            {
+                Console.WriteLine("Duel # {0}", counter++);
+                Console.WriteLine("Tank on the left:\n\n{0}\nTank on the right:\n\n{1}",pair.t34.ShowInfo(), pair.panther.ShowInfo());
+
+
+                Thread.Sleep(1500);
+                Console.WriteLine("\nBoom!\n");
+
+                Thread.Sleep(1000);
+                Console.WriteLine("Swish!\n");
+
+                Thread.Sleep(1000);
+                Console.WriteLine("Kaboom!\n");
+                Thread.Sleep(1000);
+                if ((pair.t34^pair.panther) == pair.t34)
+                {
+                    Console.WriteLine("Tank on the left won! Congratz!");
+                }
+                else
+                {
+                    Console.WriteLine("Tank on the right won! Congratz!");
+                }
+                Console.WriteLine("Press ENTER to move to the next fight");
+                Console.ReadKey();
+                Console.Clear();
+
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            List<PANTHER> Pantheras = new List<PANTHER> {
+            new PANTHER("Panther_1"),
+            new PANTHER("Panther_2"),
+            new PANTHER("Panther_3"),
+            };
+
+            List<T34> T34S = new List<T34> {
+            new T34("T34_1"),
+            new T34("T34_1"),
+            new T34("T34_1"),
+            };
+
+            TankWar(T34S, Pantheras);
+
+
+
+        }
+    }
+}
